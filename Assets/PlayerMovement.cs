96e30793cb99b22872b963f54977c989ff6aa6c8
z920804tw <z920804@gmail.com>
2023-04-20 11:21:32 +0800

    2023/4/20
     新增跳躍的功能
    a.判斷我的JumpKey是否被按下，如果有
    使用add force和transform.up來跳躍。
    b.然後新增跳躍限制，canJump，碰到地板=true，跳起來=false，如果跳起來時沒碰到地板就不能一直跳，碰到地板才能跳。
    c.丟了一些物件模型進去，小鳥、水管、草方塊。

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
 
-    
-    
+
+
     [Header("移動速度設定")]
     public float moveSpeed;
-    
+    public float jumpSpeed;
+
 
     [Header("按鍵綁定")]
     public KeyCode jumpKey = KeyCode.Space;
@@ -17,13 +18,14 @@ public class PlayerMovement : MonoBehaviour
     [Header("基本設定")]
     public Transform PlayerCamera;   // 攝影機
 
-    private float horizontalInput;   // 左右方向按鍵的數值(-1 <= X <= +1)
-    private float verticalInput;     // 上下方向按鍵的數值(-1 <= Z <= +1)
-    
+    float horizontalInput;   // 左右方向按鍵的數值(-1 <= X <= +1)
+    float verticalInput;     // 上下方向按鍵的數值(-1 <= Z <= +1)
 
-    private Vector3 moveDirection;   // 移動方向
+    bool canJump=true;
 
-    private Rigidbody rbFirstPerson; // 第一人稱物件(膠囊體)的剛體
+    Vector3 moveDirection;   // 移動方向
+
+    Rigidbody rbFirstPerson; // 第一人稱物件(膠囊體)的剛體
 
     private void Start()
     {
@@ -35,20 +37,26 @@ public class PlayerMovement : MonoBehaviour
     {
         MyInput();
         SpeedControl();
-        
+
     }
 
     private void FixedUpdate()
     {
         MovePlayer(); // 只要是物件移動，建議你放到FixedUpdate()
+
     }
 
     // 方法：取得目前玩家按方向鍵上下左右的數值
     private void MyInput()
     {
-        
+
         horizontalInput = Input.GetAxisRaw("Horizontal");
         verticalInput = Input.GetAxisRaw("Vertical");
+        //判斷如果jumpKey被按下，就執行Jump()的函式
+        if (Input.GetKeyDown(jumpKey) == true&&canJump==true)         
+        {
+            Jump();
+        }
     }
 
     private void MovePlayer()
@@ -66,11 +74,24 @@ public class PlayerMovement : MonoBehaviour
         // 如果平面速度大於預設速度值，就將物件的速度限定於預設速度值
         if (flatVel.magnitude > moveSpeed) //magnitude=方向性的速度 
         {
-            Vector3 limitedVel = flatVel.normalized* moveSpeed;
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
             rbFirstPerson.velocity = new Vector3(limitedVel.x, rbFirstPerson.velocity.y, limitedVel.z);
         }
     }
-
-
+    private void Jump()
+    {
+        //先設定Y軸設定0 ，之後再往上推。
+        //Impulse 為瞬間的推力 ，Transform.up為以Y軸移動。
+        rbFirstPerson.velocity = new Vector3(rbFirstPerson.velocity.x, 0f, rbFirstPerson.velocity.z);
+        rbFirstPerson.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);
+        canJump = false;
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
 
 }
