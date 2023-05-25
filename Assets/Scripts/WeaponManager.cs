73393de7f5f1bae39d31deae0ec00b45a6c195fd
z920804tw <z920804@gmail.com>
2023-05-25 12:04:56 +0800

    2023/5/25 槍的動畫和劍的動畫 、切換武器
    新增槍射擊的動畫和劍攻擊的簡單動畫，透過數字鍵或是滾輪來切換武器

--- Assets/Scripts/WeaponManager.cs
+++ Assets/Scripts/WeaponManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
-public class Shooting : MonoBehaviour
+public class WeaponManager : MonoBehaviour
 {
     [Header("參考物件")]
     public Camera PlayerCamera;
@@ -32,6 +32,13 @@ public class Shooting : MonoBehaviour
     public Animator fireAnimation;
     public ParticleSystem fireEffect;
 
+    [Header("武器")]
+    public GameObject[] weaponObjects;        // 武器清單
+
+    int weaponNumber = 0;                     // 目前選擇武器的順序編號
+    GameObject weaponInUse;                   // 目前選擇武器
+
+
     private void Start()
     {
         Reloading = false;
@@ -40,24 +47,11 @@ public class Shooting : MonoBehaviour
         UpdateAmmoDisplay();
         
     }
+
+
     private void Update()
     {
-        // 判斷有沒有按下左鍵
-        if (Input.GetMouseButtonDown(0) == true)
-        {
-            if (bulletLeft > 0 && Reloading == false)       //彈藥大於0且並沒有在裝填彈藥時可以Shoot
-            {
-                Shoot();
-                fireEffect.Play();                         //播放開火的特效
-            }
-           
-
-        }
-        if (Input.GetKeyDown(KeyCode.R) == true &&bulletLeft<magazineSize && Reloading == false)  //按下R 且目前彈藥數量小於總彈藥 且沒有正在裝填時可以Reload                                                                
-        {
-            Reload();
-
-        }
+        MyInput();
     }
 
     void Shoot()
@@ -71,6 +65,7 @@ public class Shooting : MonoBehaviour
         if (Physics.Raycast(ray, out hit) == true)
         {
             targetPoint = hit.point;         // 如果有打到東西，就把該物件的座標給targetPoint，射線就停在該物件的座標
+            
             Debug.Log("打到物件");
         }
         else
@@ -85,17 +80,52 @@ public class Shooting : MonoBehaviour
         currentBullet.GetComponent<Transform>().LookAt(targetPoint);        //子彈的角度以我targetPoint方向去旋轉
         
 
+
         bulletLeft--;
         UpdateAmmoDisplay();
 
         this.GetComponent<Rigidbody>().AddForce(-firPos.transform.forward * recoilForce, ForceMode.Impulse); //開槍後有後座力，方向為開火方向的相反面
 
+        fireEffect.Play();                         //播放開火的特效
         if (fireAnimation != null)
         {
             fireAnimation.SetTrigger("Fire");
         }
     }
 
+    void MyInput()
+    {
+
+        // 判斷有沒有按下左鍵
+        if (Input.GetMouseButtonDown(0) == true)
+        {
+            if (bulletLeft > 0 && Reloading == false)       //彈藥大於0且並沒有在裝填彈藥時可以Shoot
+            {
+                Shoot();
+
+            }
+
+
+        }
+        if (Input.GetKeyDown(KeyCode.R) == true && bulletLeft < magazineSize && Reloading == false)  //按下R 且目前彈藥數量小於總彈藥 且沒有正在裝填時可以Reload                                                                
+        {
+            Reload();
+
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SwitchWeapon(0, 0);
+
+        // 判斷：按下數字鍵2，切換為武器1
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SwitchWeapon(0, 1);
+
+        // 判斷：滾動滑鼠滾輪
+        if (Input.GetAxis("Mouse ScrollWheel") > 0f)      // 往前滾動
+            SwitchWeapon(1);
+        else if (Input.GetAxis("Mouse ScrollWheel") < 0f) // 往後滾動
+            SwitchWeapon(-1);
+    }
     void Reload()
     {
         Reloading = true;                      // 把Reload的狀態改成True，代表正在裝填彈藥
@@ -124,5 +154,41 @@ public class Shooting : MonoBehaviour
             }
 
         }
+    }
+
+
+    void SwitchWeapon(int _addNumber, int _weaponNumber = 0)
+    {
+        // 將武器清單全部隱藏，先一次全部隱藏，再顯示需要的武器
+        foreach (GameObject item in weaponObjects)
+        {
+            item.SetActive(false);
+        }
+
+        // switch 判斷式：以參數_addNumber判斷要怎麼切換武器
+        switch (_addNumber)
+        {
+            case 0:                                                   // _addNumber == 0，代表用按鍵直接指定武器陣列位址
+                weaponNumber = _weaponNumber;
+                break;
+            case 1:                                                   // _addNumber == 1，代表往上滾滑鼠滾輪
+                if (weaponNumber == weaponObjects.Length - 1)         // 實現循環數字，假定原本的武器陣列位址已經是最後一個武器，則將武器陣列位址設定為0
+                    weaponNumber = 0;
+                else
+                    weaponNumber += 1;
+                //weaponNumber = (weaponNumber == weaponObjects.Length - 1) ? 0 : weaponNumber += 1; // 也可以把以上的判斷式寫成這樣
+                break;
+            case -1:                                                   // _addNumber == -1，代表往下滾滑鼠滾輪
+                if (weaponNumber == 0)                                 // 實現循環數字，假定原本的武器陣列位址是第一個武器，則將武器陣列位址為清單的最後一個位址
+                    weaponNumber = weaponObjects.Length - 1;
+                else
+                    weaponNumber -= 1;
+                //weaponNumber = (weaponNumber == 0) ? weaponObjects.Length - 1 : weaponNumber -= 1; // 也可以把以上的判斷式寫成這樣
+                break;
+        }
+        weaponObjects[weaponNumber].SetActive(true);    // 顯示所指定的武器
+        weaponInUse = weaponObjects[weaponNumber];      // 設定目前所選擇的武器物件(屆時可以用來執行武器所特定的方法，下一章節會介紹)
+        fireAnimation = weaponInUse.GetComponent<Animator>();
+        
     }
 }
